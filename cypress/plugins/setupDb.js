c2c1c6b9481d54d15439d98d4a1675d7da2a817f
Eugene Petrik <eugeniy.petrik@gmail.com>
2024-11-03 16:22:44 +0200

    Update app folder name

--- cypress/plugins/setupDb.js
+++ cypress/plugins/setupDb.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const dbPath = path.resolve('./app/public/data/data.json');
+const dbPath = path.resolve('./trello_app/public/data/data.json');
 
 const empty = {
   boards: [],
